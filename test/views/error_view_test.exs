44d840e2f318727e8d2e29c7da22b309342ffa38
Joe-noh <goflb.jh@gmail.com>
2016-03-22 22:31:24 +0900

    fix test_helper for ecto 2.0

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -1,21 +1,17 @@
 defmodule Trav.ErrorViewTest do
   use Trav.ConnCase, async: true
 
-  # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(Trav.ErrorView, "404.json", []) ==
-           %{errors: %{detail: "Page not found"}}
+    assert render(Trav.ErrorView, "404.json", []) == %{errors: %{detail: "Page not found"}}
   end
 
   test "render 500.json" do
-    assert render(Trav.ErrorView, "500.json", []) ==
-           %{errors: %{detail: "Server internal error"}}
+    assert render(Trav.ErrorView, "500.json", []) == %{errors: %{detail: "Server internal error"}}
   end
 
   test "render any other" do
-    assert render(Trav.ErrorView, "505.json", []) ==
-           %{errors: %{detail: "Server internal error"}}
+    assert render(Trav.ErrorView, "505.json", []) == %{errors: %{detail: "Server internal error"}}
   end
 end
