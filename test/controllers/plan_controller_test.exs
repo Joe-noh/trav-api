fe0aa6b05a437e0ee27c3349e1f29af549e1e95b
Joe-noh <goflb.jh@gmail.com>
2016-03-26 21:42:36 +0900

    consitent controller testing

--- test/controllers/plan_controller_test.exs
+++ test/controllers/plan_controller_test.exs
@@ -14,39 +14,59 @@ defmodule Trav.PlanControllerTest do
       |> put_req_header("accept", "application/json")
       |> put_req_header("authorization", "Bearer " <> JWT.encode(%{user_id: user.id}))
 
-    {:ok, [conn: conn, user: user, trip: trip, plan: plan]}
+    {:ok, [conn: conn, trip: trip, plan: plan]}
   end
 
-  test "shows chosen resource", %{conn: conn, trip: trip, plan: plan} do
-    conn = get conn, trip_plan_path(conn, :show, trip, plan)
-    assert json_response(conn, 200)["data"] == %{"id" => plan.id,
+  test "GET a plan", %{conn: conn, trip: trip, plan: plan} do
+    response = conn
+      |> get(trip_plan_path(conn, :show, trip, plan))
+      |> json_response(200)
+
+    assert response["data"] == %{
+      "id" => plan.id,
       "body" => plan.body,
-      "trip_id" => plan.trip_id}
+      "trip_id" => plan.trip_id
+    }
   end
 
-  test "creates and renders resource when data is valid", %{conn: conn, trip: trip} do
-    conn = post conn, trip_plan_path(conn, :create, trip), plan: PlanFactory.fields_for(:plan)
-    assert json_response(conn, 201)["data"]["id"]
+  test "POST a valid plan", %{conn: conn, trip: trip} do
+    response = conn
+      |> post(trip_plan_path(conn, :create, trip), plan: PlanFactory.fields_for(:plan))
+      |> json_response(201)
+
+    assert response["data"]["id"]
   end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn, trip: trip} do
-    conn = post conn, trip_plan_path(conn, :create, trip), plan: PlanFactory.fields_for(:invalid_plan)
-    assert json_response(conn, 422)["errors"] != %{}
+  test "POST an invalid plan", %{conn: conn, trip: trip} do
+    response = conn
+      |> post(trip_plan_path(conn, :create, trip), plan: PlanFactory.fields_for(:invalid_plan))
+      |> json_response(422)
+
+    assert response["errors"] != %{}
   end
 
-  test "updates and renders chosen resource when data is valid", %{conn: conn, trip: trip, plan: plan} do
-    conn = put conn, trip_plan_path(conn, :update, trip, plan), plan: PlanFactory.fields_for(:plan)
-    assert json_response(conn, 200)["data"]["id"]
+  test "PUT a valid plan", %{conn: conn, trip: trip, plan: plan} do
+    response = conn
+      |> put(trip_plan_path(conn, :update, trip, plan), plan: PlanFactory.fields_for(:plan))
+      |> json_response(200)
+
+    assert response["data"]["id"]
   end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, trip: trip, plan: plan} do
-    conn = put conn, trip_plan_path(conn, :update, trip, plan), plan: PlanFactory.fields_for(:invalid_plan)
-    assert json_response(conn, 422)["errors"] != %{}
+  test "PUT an invalid plan", %{conn: conn, trip: trip, plan: plan} do
+    response = conn
+      |> put(trip_plan_path(conn, :update, trip, plan), plan: PlanFactory.fields_for(:invalid_plan))
+      |> json_response(422)
+
+    assert response["errors"] != %{}
   end
 
   test "deletes chosen resource", %{conn: conn, trip: trip, plan: plan} do
-    conn = delete conn, trip_plan_path(conn, :delete, trip, plan)
-    assert response(conn, 204)
+    response = conn
+      |> delete(trip_plan_path(conn, :delete, trip, plan))
+      |> response(204)
+
+    assert response
     refute Repo.get(Plan, plan.id)
   end
 end
