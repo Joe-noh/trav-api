6da0adf24b6459a3e6a891a960c18e28ece6f42f
Joe-noh <goflb.jh@gmail.com>
2016-03-26 23:56:25 +0900

    forgot to have one plan

--- web/models/trip.ex
+++ web/models/trip.ex
@@ -1,23 +1,19 @@
 defmodule Trav.Trip do
   use Trav.Web, :model
 
-  alias Trav.User
+  alias Trav.{User, Plan}
 
   schema "trips" do
     field :title, :string
+
     belongs_to :user, User
+    has_one :plan, Plan
 
     timestamps
   end
 
   @allowed ~w(title)
 
-  @doc """
-  Creates a changeset based on the `model` and `params`.
-
-  If no params are provided, an invalid changeset is returned
-  with no validation performed.
-  """
   def changeset(model, params \\ %{}) do
     model
     |> cast(params, @allowed)
